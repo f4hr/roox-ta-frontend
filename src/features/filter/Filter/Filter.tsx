85ede15f8a93c7f85f56d7a4f2ebcc38bfd843b9
Ilya Fakhrutdinov <bekamez@gmail.com>
2022-04-24 13:25:37 +0500

    feat: update filter section

--- src/features/filter/Filter/Filter.tsx
+++ src/features/filter/Filter/Filter.tsx
@@ -1,5 +1,47 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { block } from 'bem-cn';
+
+import './Filter.scss';
+import { useAppDispatch } from '../../../app/store';
+import { fieldSelector, statusSelector, setField } from '../filterSlice';
+import Button from '../../../common/Button/Button';
+
+const b = block('filter');
 
 export default function Filter() {
-  return <p>Filter</p>;
+  const dispatch = useAppDispatch();
+  const field = useSelector(fieldSelector);
+  const status = useSelector(statusSelector);
+
+  const changeField = (fieldName: string) => {
+    dispatch(setField(fieldName === field ? '' : fieldName));
+  };
+
+  return (
+    <div className={b()}>
+      <ul className={b('list')}>
+        <li className={b('item')}>
+          <Button
+            state={field === 'city' ? 'active' : ''}
+            mod="main"
+            onClick={() => changeField('city')}
+            disabled={status === 'disabled'}
+          >
+            по городу
+          </Button>
+        </li>
+        <li className={b('item')}>
+          <Button
+            state={field === 'company' ? 'active' : ''}
+            mod="main"
+            onClick={() => changeField('company')}
+            disabled={status === 'disabled'}
+          >
+            по компании
+          </Button>
+        </li>
+      </ul>
+    </div>
+  );
 }
